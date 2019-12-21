976540de0b72b0645fee30ccf22e2a9dbcbc8363
Jitendra Sachdeva <jitensachdeva@gmail.com>
2019-12-21 10:27:17 +0530

    Updates

---
+++ precache-manifest.f75d083566c4f9dbe4b050655c5a5348.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05927c9c7bdd60ec2d0a084059cef950",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "2205896cd962254ecd95",
+    "url": "/react-tutorial/static/css/main.2646eaca.chunk.css"
+  },
+  {
+    "revision": "3da17989d5c5da24ec10",
+    "url": "/react-tutorial/static/js/2.47b8276d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-tutorial/static/js/2.47b8276d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2205896cd962254ecd95",
+    "url": "/react-tutorial/static/js/main.6525d953.chunk.js"
+  },
+  {
+    "revision": "8d76b84dd1ea32b60fde",
+    "url": "/react-tutorial/static/js/runtime-main.f01b29f8.js"
+  }
+]);
