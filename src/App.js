a96060789319f377320b521bcee9d14aa7e5a703
Jitendra Sachdeva <jitensachdeva@gmail.com>
2019-12-21 08:15:28 +0530

    Added form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 class App extends Component {
     state = {
@@ -42,6 +43,7 @@ class App extends Component {
         return (
             <div className="container">
                <Table characterData={characters} removeCharacter={this.removeCharacter}/>
+               <Form/>
             </div>
         )
     }
---
+++ src/Form.js
@@ -0,0 +1,43 @@
+import React, {Component} from "react";
+
+class Form extends Component{
+    constructor(props) {
+        super(props);
+
+        this.initialState = {
+            name: '',
+            job: '',
+        }
+
+        this.state = this.initialState
+    }
+
+    handleChange = event => {
+        const { name, job } = event.target
+        this.setState({
+            [name] : job
+        })
+    }
+
+    render(){
+        const { name, job } = this.state
+
+        return(
+            <form>
+            <label>Name</label>
+            <input
+                type="text"
+                name="name"
+                value={name}
+                onChange={this.handleChange} />
+            <label>Job</label>
+            <input
+                type="text"
+                name="job"
+                value={job}
+                onChange={this.handleChange} />
+        </form>)
+    }
+
+}
+export default Form
