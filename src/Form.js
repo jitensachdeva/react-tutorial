b0c59bb3529ea7b5b580312a145937ef7ef29a8b
Jitendra Sachdeva <jitensachdeva@gmail.com>
2019-12-21 08:41:28 +0530

    Submit button

--- src/Form.js
+++ src/Form.js
@@ -19,6 +19,11 @@ class Form extends Component{
         })
     }
 
+    submitForm = () => {
+        this.props.handleSubmit(this.state)
+        this.setState(this.initialState)
+    }
+
     render(){
         const { name , job } = this.state
 
@@ -36,6 +41,8 @@ class Form extends Component{
                 name="job"
                 value={job}
                 onChange={this.handleChange} />
+
+            <input type="button" value="Submit" onClick={this.submitForm} />
         </form>)
     }
 
